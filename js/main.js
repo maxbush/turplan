0e527bcfe967d04a1294bbcde94d1a311f4d32d8
maxbush <maxim.bushenko@gmail.com>
2022-06-16 16:04:42 +0300

    Added slider

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next1',
+    prevEl: '.swiper-button-prev1',
+  },
+//   effect: 'cube',
+ 
+});
